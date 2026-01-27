177ca6010596330443133ac848cc0bee4158661a
chapppington <stas.mega.96@bk.ru>
2026-01-27 14:17:54 +0700

    (feat) media upload endpoint now supports any amount of files

--- app/infrastructure/integrations/bitrix/__init__.py
+++ app/infrastructure/integrations/bitrix/__init__.py
@@ -2,11 +2,9 @@ from infrastructure.integrations.bitrix.client import (
     BitrixClient,
     BitrixLeadData,
 )
-from infrastructure.integrations.bitrix.converter import convert_event_to_lead_data
 
 
 __all__ = [
     "BitrixClient",
     "BitrixLeadData",
-    "convert_event_to_lead_data",
 ]

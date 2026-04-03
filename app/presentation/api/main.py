49b47ac77de712a31cccf12195223a07e825f4c6
chapppington <stas.mega.96@bk.ru>
2026-04-03 12:28:05 +0700

    chore: dev cors fixes

--- app/presentation/api/main.py
+++ app/presentation/api/main.py
@@ -18,7 +18,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://sibkomplekt.ru", "https://www.sibkomplekt.ru", "https://admin.sibkomplekt.ru"],
+        allow_origins=["http://localhost:3000", "http://localhost:8000"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
